417447f6c666e8353c2567e3b3f318b4eb644992
Shehzad Aslam Oza <shehzadaslamoza@gmail.com>
2022-02-04 19:15:15 +0500

    Added Structure to the UI

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 let playerScoreBoard = document.querySelector("#playerScore");
 let computerScoreBoard = document.querySelector("#compScore");
 let gameDetail = document.querySelector(".gameDetail");
+let gameStatus = document.querySelector(".weaponMessage");
 let playerPoints = 0;
 let computerPoints = 0;
 
@@ -9,13 +10,11 @@ let playAgainButton = document.querySelector(".playAgainBtn");
 playAgainButton.addEventListener("click", () => {
   playerPoints = 0;
   computerPoints = 0;
-  message = "";
-  updateUI(message);
+  updateUI("Choose Your Weapon", "First to Score 5 points Wins");
 
   rockBtn.disabled = false;
   paperBtn.disabled = false;
   scissorBtn.disabled = false;
-  playAgainButton.disabled = true;
 });
 
 // Player Chooses Rock
@@ -29,14 +28,14 @@ rockBtn.addEventListener("click", () => {
 let paperBtn = document.querySelector(".paperBtn");
 paperBtn.addEventListener("click", () => {
   console.log("Paper Btn Pressed");
-  playRound(playerPlay(rockBtn.textContent), computerPlay());
+  playRound(playerPlay(paperBtn.textContent), computerPlay());
 });
 
 // Player Chooses Scissor
 let scissorBtn = document.querySelector(".scissorBtn");
 scissorBtn.addEventListener("click", () => {
   console.log("Scissor Btn Pressed");
-  playRound(playerPlay(rockBtn.textContent), computerPlay());
+  playRound(playerPlay(scissorBtn.textContent), computerPlay());
 });
 
 // Computer Move
@@ -60,48 +59,50 @@ function playerPlay(playerMove) {
 
 // One Round
 function playRound(playerSelection, computerSelection) {
-  let message;
+  let message1;
+  let message2;
 
   if (playerSelection == computerSelection) {
     console.log(`Game Tied! Both Chose ${playerSelection}`);
-    message = `Game Tied! Both Chose ${playerSelection}`;
-  }
-
-  if (
+    message1 = `Both Chose ${playerSelection}`;
+    message2 = "Game Tied!";
+  } else if (
     (playerSelection == "rock" && computerSelection == "scissors") ||
     (playerSelection == "paper" && computerSelection == "rock") ||
     (playerSelection == "scissors" && computerSelection == "paper")
   ) {
     console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
     playerPoints++;
-    message = `You Win! ${playerSelection} beats ${computerSelection}`;
+    message1 = `${playerSelection} beats ${computerSelection}`;
+    message2 = "You Win!";
   } else {
     console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
     computerPoints++;
-    message = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    message1 = `${computerSelection} beats ${playerSelection}`;
+    message2 = "You Lose!";
   }
 
-  updateUI(message);
+  updateUI(message1, message2);
   checkGameOver();
 }
 
-function updateUI(message) {
+function updateUI(message1, message2) {
   computerScoreBoard.textContent = computerPoints;
   playerScoreBoard.textContent = playerPoints;
-  gameDetail.textContent = message;
+  gameDetail.textContent = message1;
+  gameStatus.textContent = message2;
 }
 
 function checkGameOver() {
   if ((computerPoints == 5) | (playerPoints == 5)) {
     if (computerPoints == 5) {
-      updateUI("Computer Wins");
+      document.querySelector(".weaponMessage").textContent = "Computer Win";
     } else if (playerPoints == 5) {
-      updateUI("You Win");
+      document.querySelector(".weaponMessage").textContent = "You Win";
     }
 
     rockBtn.disabled = true;
     paperBtn.disabled = true;
     scissorBtn.disabled = true;
-    playAgainButton.disabled = false;
   }
 }
